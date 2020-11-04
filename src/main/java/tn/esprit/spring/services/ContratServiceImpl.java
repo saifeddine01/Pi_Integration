1786f7413ba04eb6ddacf83c27b04e508177b19a
mnasri akrem <akrem.mnasri@esprit.tn>
2020-11-04 21:08:37 +0100

    succés

--- src/main/java/tn/esprit/spring/services/ContratServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/ContratServiceImpl.java
@@ -2,6 +2,7 @@ package tn.esprit.spring.services;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,13 +11,26 @@ import tn.esprit.spring.repository.ContratRepository;
 
 @Service
 public class ContratServiceImpl implements IContratService {
-
-
 	@Autowired
 	ContratRepository contratRepository;
+	private static final Logger l = Logger.getLogger(ContratServiceImpl.class);
 
+	
+	public Contrat addContrat(Contrat contrat) {
+		contratRepository.save(contrat);
+		l.info("ajout du contrat avec succes"+contrat);
+		return contrat;
+	}
 
+	public Contrat findcontrat(int ref) {
+		return contratRepository.findById(ref).orElse(null);
+	}
+	public void deleteContrat(Contrat contrat) {
+		contratRepository.delete(contrat);
+		l.info("supprimer le contrat avec succes"+contrat);
+	}
 	public List<Contrat> getAllContrats() {
+		
 		return (List<Contrat>) contratRepository.findAll();
 	}
 
