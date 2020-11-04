427ccaa2ee09e27804fba2c665828ad51c876363
Jassem003 <jassem.benrayana@esprit.tn>
2020-11-04 23:31:34 +0100

    Added Test Affection Contract to Employe + Test Get All Employees + Test Count Employees

--- src/test/java/tn/esprit/spring/EmployeTest.java
+++ src/test/java/tn/esprit/spring/EmployeTest.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import tn.esprit.spring.entities.Departement;
 import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.entities.Role;
 import tn.esprit.spring.services.IEmployeService;
 
 import java.text.ParseException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -31,8 +33,38 @@ public class EmployeTest {
 	
 	@Test
 	public void TestAddorUpdate() throws ParseException {
+		logger.info("-------Starting Add or Update--------"); 
 		Employe u = new Employe("Jassem", "Ben Rayana","jassem@gmail.com","123456", true, Role.TECHNICIEN);
 		int EmployeID = es.addOrUpdateEmploye(u); 
+		logger.info("ID of Employe is :" + EmployeID);
 		assertEquals(EmployeID, u.getId());
+		logger.info("-------Finished Add or Update--------"); 
+	}
+	
+	@Test
+	public void TestGetEmployeNumber() {
+		logger.info("-------Starting Counting Employees-------");
+		es.getNombreEmployeJPQL();
+		logger.info("-------Finished Counting of Employees--------"); 
+	}
+	
+	@Test
+	public void TestGetAllEmployees()  {
+		logger.info("-------Get all Employees Starting-------");
+		List<Employe> listEmploye = es.getAllEmployes(); 
+		logger.info("Employees Are : " + listEmploye);
+		logger.info("-------Get all Employees Finished-------");
+	}
+	
+	@Test
+	public void TestAffecterContractToEmploye()  {
+		logger.info("-------Affaction Contract to Employe Starting-------");
+		int contratID = 1;
+		int EmployeID = 5;
+		es.affecterContratAEmploye(contratID, EmployeID);
+		
+		logger.info("Contrat with ID "+ contratID + " is affected to Employe with ID " + EmployeID );
+		
+		logger.info("-------Affaction Contract to Employe Finished-------");
 	}
 }
